f99cb7f410407109b2e9c19d00b4baa9e3966d4c
Noam Shani <Noams156@gmail.com>
2022-02-26 22:51:11 +0100

    first draft

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './home.scss'
+
+
+
+export default function home() {
+
+    return (
+
+        <div className='home'>
+
+            <img className='urban' src={require('../img/urbanlife.gif')} alt='urbanlife' height={900} width={1700} />
+
+        </div>
+
+    )
+}
+
